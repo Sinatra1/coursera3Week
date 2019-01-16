ce0d8cc77731548cdce47e0acbd3365223fb5d7e
Vladislav <shumilov@simpls.ru>
2019-01-16 11:51:09 +0500

    3 part of 3 week task (open browser)

--- app/src/main/java/com/shumilov/vladislav/myfirstapp/SecondActivity.java
+++ app/src/main/java/com/shumilov/vladislav/myfirstapp/SecondActivity.java
@@ -2,11 +2,17 @@ package com.shumilov.vladislav.myfirstapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class SecondActivity extends AppCompatActivity {
     private TextView mTextView;
@@ -41,5 +47,42 @@ public class SecondActivity extends AppCompatActivity {
         if (mTextView != null && !TextUtils.isEmpty(text)) {
             mTextView.setText(text);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mButtonView == null || mTextView == null) {
+            return;
+        }
+
+        mButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchInBrowser(mTextView.getText().toString());
+            }
+        });
+    }
+
+    private void searchInBrowser(String query) {
+        if (TextUtils.isEmpty(query)) {
+            return;
+        }
+
+        String escapedQuery = "";
+
+        try {
+            escapedQuery = URLEncoder.encode(query, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            Log.e(SecondActivity.class.getSimpleName(), e.getMessage());
+        }
+
+        String url = "http://google.com/#q=" + escapedQuery;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
     }
 }
